75f230509ae42735a1072c8199a21417a749c318
alexandt <alexandt96@gmail.com>
2020-08-20 19:21:03 -0500

    working for me

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const axios = require('axios')
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  url = 'https://raider.io/api/v1/characters/profile?region=us&realm=sargeras&name=sehni&fields=mythic_plus_scores_by_season%3Acurrent'
+  axios.get(url).then(async (data)=> {
+    const parsedData = data.data
+    const season = parsedData.mythic_plus_scores_by_season.find(score => score.season === 'season-bfa-4')
+    console.log(season.scores.all)
+    res.send('done!')
+  }).catch((e)=>{
+    next(e)
+  })
+
 })
 
 app.listen(port, () => {
